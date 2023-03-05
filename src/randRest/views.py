83db6af6e6e136da8f6613c88ec08eac19d9ca50
Justinchunng@gmail.com <Justinchunng@gmail.com>
2023-03-04 20:02:20 -0500

    Completed signup mechanism

--- src/randRest/views.py
+++ src/randRest/views.py
@@ -13,7 +13,7 @@ def login(request):
 
 def signup(request):
     if request.method == "POST":
-        errors = []
+        errors = 0
         first_name = request.POST["firstName"]
         last_name = request.POST["lastName"]
         username = request.POST["username"]
@@ -21,21 +21,26 @@ def signup(request):
         password1 = request.POST["password1"]
         password2 = request.POST["password2"]
         if len(username) < 5:
-            errors.append("Username is not long enough")
+            messages.add_message(request, messages.ERROR, "Username is not long enough")
+            errors += 1
         if User.objects.filter(username=username).exists():
-            errors.append("Username is taken")
+            messages.add_message(request, messages.ERROR, "Username is taken")
+            errors += 1
         if User.objects.filter(email=email).exists():
-            errors.append("Email is taken")
+            messages.add_message(request, messages.ERROR, "Email is taken")
+            errors += 1
         if password1 != password2:
-            errors.append("Passowrds do not match")
+            messages.add_message(request, messages.ERROR, "Passwords do not match")
+            errors += 1
         
-        if len(errors) > 0:
-            return render(request, "signup.html", {"messages": errors})
+        if errors > 0:
+            return render(request, "signup.html")
         
         user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
         user.save()
         print('user created!')
-        return redirect('/home')
+        messages.add_message(request, messages.SUCCESS, "Account successfully created!")
+        return render(request, "signup.html")
     return render(request, "signup.html")    
 
 def home(request):
